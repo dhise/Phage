5c408341d729d0efc52e329f6d2a22e40a24d83b
dhise <jdhise@gmail.com>
2025-03-15 22:52:45 -0400

    Add basic movement, player shape, background color

--- main.cpp
+++ main.cpp
@@ -3,10 +3,26 @@
 #include <typeinfo>
 #include <memory>
 
+
+
+
+
+
+
 int main()
 {
+
+
+
     // create the window
     sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "My window");
+
+    //simple player shape
+    sf::RectangleShape player({ 120.f, 50.f });
+    player.setFillColor(sf::Color(0, 255, 0));
+    player.setPosition({ 200, 150 });
+
+
 
     // run the program as long as the window is open
     while (window.isOpen())
@@ -19,12 +35,29 @@ int main()
                 // close window when Q is pressed
                 window.close();
             }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+            {
+                player.move({ 0.f, 1.0f });
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+            {
+                player.move({ 0.f, -1.0f });
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+            {
+                player.move({ -1.f, 0.f });
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+            {
+                player.move({ 1.0f, 0.f });
+            }
+         
            
         }
         //clear the window with black
         window.clear(sf::Color::Black);
         
-        //window.draw();
+        window.draw(player);
 
         //End the current frame
         window.display();
